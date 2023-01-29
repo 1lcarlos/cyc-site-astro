683e0dcc3f0d9a98e241d3c1048d9796f7f60ee1
lcarlos <luiscardozoicg@gmail.com>
2023-01-28 22:21:38 -0500

    integration tailwind with astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://1lcarlos.github.io',
-    base: '/cyc-site-astro',
+  site: 'https://1lcarlos.github.io',
+  base: '/cyc-site-astro',
+  integrations: [tailwind()]
 });
