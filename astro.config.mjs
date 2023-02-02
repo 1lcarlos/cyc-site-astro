4d74ce416b32f3bfb272d82889440520b4047a89
lcarlos <luiscardozoicg@gmail.com>
2023-02-02 12:55:08 -0500

    add astro-node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://1lcarlos.github.io',
   base: '/cyc-site-astro',
+  output: "server",
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [tailwind()]
 });
