39e01b5b6087b3e1ed6e9f37262bb553017cf726
lcarlos <luiscardozoicg@gmail.com>
2023-02-02 13:04:53 -0500

    delete node config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import node from "@astrojs/node";
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -10,9 +10,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: 'https://1lcarlos.github.io',
   base: '/cyc-site-astro',
-  output: "server",
-  adapter: node({
-    mode: 'standalone'
-  }),
   integrations: [tailwind()]
 });
